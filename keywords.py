521c7cc46a4e86b1991556a199b3d40683c5a651
amykaggle <amykaggle@gmail.com>
2014-11-01 08:53:45 -0400

    document parsing complete

--- keywords.py
+++ keywords.py
@@ -1,7 +1,13 @@
 import nltk
 import helper
 
-
+def split_word(word):
+	words=word.split('/')
+	return words
+#	results=[]
+#	for word in words:
+#		results.extend(word.split('-'))
+#	return results
 def preprocess(sent):
 	sent = nltk.word_tokenize(sent)
 #	grammar = r"""
@@ -14,7 +20,7 @@ def preprocess(sent):
 
 	grammar = r"""
 	NBAR:
-	{<NN.*|JJ>*<NN.*>} # Nouns and Adjectives, terminated with Nouns
+	{<NN.*|JJ|CD>*<NN.*>} # Nouns and Adjectives, terminated with Nouns
 	NP:
 	{<NBAR>}
 	{<NBAR><IN><NBAR>} # Above, connected with in/of/etc...
@@ -22,18 +28,23 @@ def preprocess(sent):
 	sent=nltk.pos_tag(sent)
 	cp = nltk.RegexpParser(grammar)
 	result = cp.parse(sent)
-	print sent
-	print result
-	print result.subtrees
+	#print sent
+	#print result
+	#print result.subtrees
 	terms = helper.get_terms(result)
 
+	kw=[]
 	for term in terms:
 	    for word in term:
 		print word,
+		ws=split_word(word)
+		for w in ws:
+			kw.append(w)
 	    print
-	print "\n\n\n"
+
+	return kw
 
 if __name__=="__main__":
 	#sent="And now for something completely different"
 	sent="can you give me a 12 month quote"
-	preprocess(sent)
+	print preprocess(sent)
